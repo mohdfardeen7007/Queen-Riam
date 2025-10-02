c00022f1d97c8faa2d9ff1b5ad27ba23b7874807
mohdfardeen7007 <162273323+mohdfardeen7007@users.noreply.github.com>
2025-10-02 21:35:17 +0530

    Update song.js

--- commands/song.js
+++ commands/song.js
@@ -38,7 +38,7 @@ async function songCommand(sock, chatId, message) {
         // Send video info before download
         await sock.sendMessage(chatId, {
             image: { url: video.thumbnail },
-            caption: ` *${video.title}*\n\n`
+            caption: `🌟 *${video.title}*\n\n`
         }, { quoted: message });
 
         // React ⏳ while downloading
