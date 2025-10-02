3a284edd995834ec3fe24578a1210e957846396b
mohdfardeen7007 <162273323+mohdfardeen7007@users.noreply.github.com>
2025-10-02 20:06:42 +0530

    Update tagall.js

--- commands/tagall.js
+++ commands/tagall.js
@@ -15,13 +15,13 @@ async function tagAllCommand(sock, chatId, senderId) {
         const totalMembers = participants.length;
 
         // ✨ Add announcement message here
-        const announcement = "🧭ᩨ─ *𝐀𐓣𐓣ⱺυ𐓣𝖼𝖾ꭑ𝖾𐓣𝗍:*\n𝐖α𝗄𝖾 𝐔ρ 𝐄𝗏𝖾𝗋𝗒ⱺ𐓣\n";
+        const announcement = "🧭ᩨ─ *𝐀𐓣𐓣ⱺυ𐓣𝖼𝖾ꭑ𝖾𐓣𝗍:*\n𝐖α𝗄𝖾 𝐔ρ 𝐄𝗏𝖾𝗋𝗒ⱺ𐓣e\n";
 
         // Create message
         let message = `━﹝̣ׄ🩰̸̶ּׄ͜﹞ *𝐆𝖼 𝐍αꭑ𝖾:* ${groupName}\n━﹝̣ׄ🌟̸̶ּׄ͜﹞ *𝐓ⱺ𝗍αᥣ 𝐌𝖾ꭑᑲ𝖾𝗋𝗌:* ${totalMembers}\n\n${announcement}\n🔊 *𝐓α𝗀𝗀𝖾ᑯ 𝐌𝖾ꭑᑲ𝖾𝗋𝗌:*\n\n`;
 
         participants.forEach(participant => {
-            message += `@${participant.id.split('@')[0]}\n`;
+            message += `@${participant.id.split(━ ✦ ⃞🥮ᩧᩙᩪᩩ̶̷  ͟ ͟ ͟ ͟'@')[0]}\n`;
         });
 
         // Send message with mentions
